4ab28182bcb12682ccecfc118484f5ff0292b70b
sowly <sowly@DESKTOP-8VROF2V>
2024-02-28 17:22:45 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var car in new CarManager(new InMemoryCarDal()).GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
